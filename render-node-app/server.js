c27bbde570b6ca4a21939edaa276a19eb50e4951
Yehudit-E <y0556782301@gmail.com>
2025-02-06 23:43:44 +0200

    update node 2

--- render-node-app/server.js
+++ render-node-app/server.js
@@ -32,35 +32,35 @@
 // });
 
 
-// import express from 'express';
-// import renderApi from '@api/render-api';
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // אימות מול Render API
-// renderApi.auth('rnd_Tbh8fS2S0sMDwG6ESa4UdquGvRwK');
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const { data } = await renderApi.listServices({ includePreviews: 'true', limit: '20' });
-//     res.json(data);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send('Error fetching data from Render API');
-//   }
-// });
-
-// // הפעלת השרת
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
+import express from 'express';
 import renderApi from '@api/render-api';
 
-renderApi.auth('rnd_jAtpseriLSvEa0zJFp7pgwwjnNUE');
-renderApi.listServices({includePreviews: 'true', limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// אימות מול Render API
+renderApi.auth('rnd_Tbh8fS2S0sMDwG6ESa4UdquGvRwK');
+
+app.get('/', async (req, res) => {
+  try {
+    const { data } = await renderApi.listServices({ includePreviews: 'true', limit: '20' });
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error fetching data from Render API');
+  }
+});
+
+// הפעלת השרת
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// import renderApi from '@api/render-api';
+
+// renderApi.auth('rnd_jAtpseriLSvEa0zJFp7pgwwjnNUE');
+// renderApi.listServices({includePreviews: 'true', limit: '20'})
+//   .then(({ data }) => console.log(data))
+//   .catch(err => console.error(err));
